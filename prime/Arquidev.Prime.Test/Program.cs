4b5d8b78efce6db2dac5d7180d7a65be6ea901d5
queil <4584075+queil@users.noreply.github.com>
2026-03-20 14:18:51 +0000

    fix(prime): rename weather endpoint to /api/weather

--- prime/Arquidev.Prime.Test/Program.cs
+++ prime/Arquidev.Prime.Test/Program.cs
@@ -26,7 +26,7 @@ var summaries = new[]
     "Arctic", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/api/weather", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
