9ca85c3b2db8b3f174c156911e6926043db22dc1
queil <4584075+queil@users.noreply.github.com>
2026-03-20 14:18:51 +0000

    feat(prime): add Arctic to summaries

--- prime/Arquidev.Prime.Test/Program.cs
+++ prime/Arquidev.Prime.Test/Program.cs
@@ -23,7 +23,7 @@ app.UseRouting();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Arctic", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
